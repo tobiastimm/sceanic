1db7b340f1e998025e292a985250e90a5c1b2c8b
Tobias Timm <tbs.timm@gmail.com>
2018-09-09 16:54:43 +0200

    First preview

---
+++ README.md
@@ -0,0 +1,36 @@
+# 手裏剣 Shuriken
+
+[![Version](https://vsmarketplacebadge.apphb.com/version/TobiasTimm.shuriken.svg)](https://marketplace.visualstudio.com/items?itemName=TobiasTimm.shuriken)
+[![Downloads](https://img.shields.io/vscode-marketplace/d/TobiasTimm.shuriken.svg)](https://marketplace.visualstudio.com/items?itemName=TobiasTimm.shuriken)
+
+[**手裏剣 Shuriken**](https://tobiastimm.github.io/shuriken/) is an opinionated vscode theme inspired by spacegray and oceanic next.
+
+![Screenshot](screenshot.png)
+
+See the [CHANGELOG](CHANGELOG.md) for the latest changes.
+
+## Usage
+
+Select the theme and you are ready to go!
+
+## Installation
+
+1.  Open Extensions sidebar panel in VS Code. View → Extensions
+1.  Search for `Shuriken`
+1.  Click Install
+1.  Click Reload
+1.  File > Preferences > Color Theme > Shuriken
+
+## Feedback / Issues
+
+Do you like the theme? Don't hesitate to share your thoughts!
+
+Let me know, if you'd like to see any specific synytax added by creating a [Github issue](https://github.com/tobiastimm/shuriken/issues).
+
+## Author
+
+[Tobias Timm](https://twitter.com/TbsTimm)
+
+## License
+
+MIT
---
+++ demo/sample.jsx
@@ -0,0 +1,68 @@
+// @flow
+
+import * as React from 'react';
+
+interface Sample {
+    type: string | undefined;
+    name?: string;
+    click(): void;
+    callback: () => void;
+}
+
+type SampleType = {
+    myType: string;
+    callback: () => void;
+};
+
+export class Test extends React.Component<{ bar: string }, {}>{
+    render() {
+        return (
+            <div foo={this.props.bar} bar={true} baz={'str'} boz={"other str"}></div>
+        );
+    }
+}
+
+
+const reg = /\.(js|ts|mjs)$/;
+
+const bla = true === "bla" ? "blub" : "foo";
+
+for (var i = test; i < 10; i++) { }
+
+while (true) { }
+
+do { } while (true);
+
+import * as styles from './mission.scss';
+
+// this is a comment!
+export default function MissionIcon({ key, title }) {
+
+    const string = 'string';
+    const number = 20;
+    const blue = true;
+
+    return <i class={classnames(
+        styles['mission__icon'],
+        {
+            [styles['mission__icon-letter']]: title.match(A_HEBREW_LETTER),
+            [styles['mission__icon-first']]: !key,
+        }
+    )}>{title}</i>;
+}
+
+@connect(
+    ({ artists }) => ({ artists }),
+    dispatch => bindActionCreators({ getArtists, getArtist, addArtist, editArtist, removeArtist }, dispatch)
+)
+export default class AdminArtists extends Component {
+
+    componentWillMount = () => {
+        this.props.getArtists();
+    }
+
+    render = () => {
+        console.log(this.props.artists);
+        return <div><div><div></div></div></div>;
+    }
+}
